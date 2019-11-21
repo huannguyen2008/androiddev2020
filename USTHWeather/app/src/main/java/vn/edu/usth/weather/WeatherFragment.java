462433189163914906f70952186ea440170c598a
huannguyen2008 <huannguyen2299@gmail.com>
2019-11-21 10:06:24 +0700

    Add missing files for labwork6

---
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherFragment.java
@@ -0,0 +1,18 @@
+package vn.edu.usth.weather;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class WeatherFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_weather_fragment,container,false);
+    }
+}
